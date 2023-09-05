5250fee2cdf1e4fa7ba835b1e9b085ff2cafc5ba
Svin07 <svin07@ukr.net>
2023-09-05 12:16:27 +0300

    commit_01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      Домашня робота №2 завдання 1
     </div>
   );
 };
